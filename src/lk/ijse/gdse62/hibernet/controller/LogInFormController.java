dd0ef1fb034c31cd0b51abdfd448c04e9f62444f
jayani018 <jayaniarunika112gmail.com>
2023-03-28 13:48:52 +0530

    Create login page

--- src/lk/ijse/gdse62/hibernet/controller/LogInFormController.java
+++ src/lk/ijse/gdse62/hibernet/controller/LogInFormController.java
@@ -1,5 +1,13 @@
 package lk.ijse.gdse62.hibernet.controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
 public class LogInFormController {
 
+    public JFXTextField txtName;
+    public JFXTextField txtPassword;
+
+    public void loginOnAction(ActionEvent actionEvent) {
+    }
 }
