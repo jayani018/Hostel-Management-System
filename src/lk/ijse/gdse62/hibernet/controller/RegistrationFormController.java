23231e66b1672c1e065a758c3cc2165dfb11d8d9
jayani018 <jayaniarunika112gmail.com>
2023-03-29 14:04:23 +0530

    Create Registration  Form

---
+++ src/lk/ijse/gdse62/hibernet/controller/RegistrationFormController.java
@@ -0,0 +1,51 @@
+package lk.ijse.gdse62.hibernet.controller;
+
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class RegistrationFormController {
+    public JFXTextField RegID;
+    public JFXTextField RegNo;
+    public JFXTextField Status;
+    public TextField search;
+    public TableView Table;
+    public TableColumn tblRID;
+    public TableColumn tblRegNo;
+    public TableColumn tblSID;
+    public TableColumn tblDate;
+    public TableColumn tblRNo;
+    public TableColumn tblstatus;
+    public TableColumn tblUniName;
+    public JFXTextField UniName;
+    public JFXTextField Date;
+    public JFXComboBox cmbStudentId;
+    public JFXComboBox CombRoomNo;
+
+    public void RoomsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void regOnAction(ActionEvent actionEvent) {
+    }
+
+    public void StudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void LogoutOnAction(ActionEvent actionEvent) {
+    }
+
+    public void SearchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void UpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void DeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void AddOnAction(ActionEvent actionEvent) {
+    }
+}
