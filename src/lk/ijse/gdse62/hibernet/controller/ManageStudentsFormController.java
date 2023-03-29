9408e5d686776236d1c80c38088bd614cb158d6a
jayani018 <jayaniarunika112gmail.com>
2023-03-29 12:45:43 +0530

    Create ManageStudentsForm

---
+++ src/lk/ijse/gdse62/hibernet/controller/ManageStudentsFormController.java
@@ -0,0 +1,52 @@
+package lk.ijse.gdse62.hibernet.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ManageStudentsFormController {
+    public JFXTextField StudentID;
+    public JFXTextField IDno;
+    public JFXTextField StudentName;
+    public JFXTextField Gender;
+    public TextField search;
+    public TableView Table;
+    public TableColumn tblSID;
+    public TableColumn tblName;
+    public TableColumn tblAddress;
+    public TableColumn tblContact;
+    public TableColumn tblID;
+    public TableColumn tblGender;
+    public TableColumn tblUniName;
+    public TableColumn tbleBID;
+    public JFXTextField UniName;
+    public JFXTextField BID;
+    public JFXTextField ContactNO;
+    public JFXTextField Address;
+
+    public void RoomsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void regOnAction(ActionEvent actionEvent) {
+    }
+
+    public void StudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void LogoutOnAction(ActionEvent actionEvent) {
+    }
+
+    public void SearchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void UpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void DeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void AddOnAction(ActionEvent actionEvent) {
+    }
+}
