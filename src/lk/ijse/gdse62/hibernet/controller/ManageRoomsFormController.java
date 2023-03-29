444be288d45c1fad37ab2a22ddbcfae6701e9cea
jayani018 <jayaniarunika112gmail.com>
2023-03-29 11:23:47 +0530

    Create Manage Rooms Form

---
+++ src/lk/ijse/gdse62/hibernet/controller/ManageRoomsFormController.java
@@ -0,0 +1,35 @@
+package lk.ijse.gdse62.hibernet.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
+public class ManageRoomsFormController {
+    public JFXTextField RoomNo;
+    public JFXTextField KeyMoney;
+    public JFXTextField Roomtype;
+    public JFXTextField RoomsQTY;
+
+    public void SearchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void UpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void DeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void AddOnAction(ActionEvent actionEvent) {
+    }
+
+    public void RoomsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void regOnAction(ActionEvent actionEvent) {
+    }
+
+    public void StudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void LogoutOnAction(ActionEvent actionEvent) {
+    }
+}
