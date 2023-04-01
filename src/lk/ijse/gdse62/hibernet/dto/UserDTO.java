9c65d5ea1d8c42d1195a550c0daf20f4112aff8f
jayani018 <jayaniarunika11@gmail.com>
2023-04-02 03:01:07 +0530

    Update DTO

--- src/lk/ijse/gdse62/hibernet/dto/UserDTO.java
+++ src/lk/ijse/gdse62/hibernet/dto/UserDTO.java
@@ -1,38 +1,18 @@
 package lk.ijse.gdse62.hibernet.dto;
 
+import javafx.beans.NamedArg;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class UserDTO {
     private String userName;
     private String password;
 
-    public UserDTO() {
-    }
-
-    public UserDTO(String userName, String password) {
-        this.userName = userName;
-        this.password = password;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "UserDTO{" +
-                "userName='" + userName + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
