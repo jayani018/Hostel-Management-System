faf030b464db28f9f0566078c228405a816a98d9
jayani018 <jayaniarunika112gmail.com>
2023-04-01 09:19:48 +0530

    Content Updated

--- src/lk/ijse/gdse62/hibernet/controller/HomeFormController.java
+++ src/lk/ijse/gdse62/hibernet/controller/HomeFormController.java
@@ -1,9 +1,21 @@
 package lk.ijse.gdse62.hibernet.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HomeFormController {
-    public void RoomsOnAction(ActionEvent actionEvent) {
+    public AnchorPane MainPane;
+    public AnchorPane dashPane;
+
+    public void RoomsOnAction(ActionEvent actionEvent) throws IOException {
+//        Stage stage = (Stage) pane.getScene().getWindow();
+//        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/lk/ijse/gdse62/hibernet/view/ManageRoomsForm.fxml"))));
+//        stage.centerOnScreen();
     }
 
     public void regOnAction(ActionEvent actionEvent) {
