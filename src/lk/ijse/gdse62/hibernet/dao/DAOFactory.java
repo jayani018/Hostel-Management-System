1957f5847512355d1fde2fd940ede57c8d1f2478
jayani018 <jayaniarunika11@gmail.com>
2023-04-02 22:12:13 +0530

    Create DAOFactory

---
+++ src/lk/ijse/gdse62/hibernet/dao/DAOFactory.java
@@ -0,0 +1,31 @@
+package lk.ijse.gdse62.hibernet.dao;
+
+import lk.ijse.gdse62.hibernet.dao.custom.impl.ReservationDAOImpl;
+import lk.ijse.gdse62.hibernet.dao.custom.impl.RoomDAOImpl;
+import lk.ijse.gdse62.hibernet.dao.custom.impl.StudentDAOImpl;
+import lk.ijse.gdse62.hibernet.dao.custom.impl.UserDAOImpl;
+
+public class DAOFactory {
+    public static DAOFactory daoFactory;
+
+    private DAOFactory(){
+
+    }
+    public static DAOFactory getInstance(){
+        return daoFactory == null ? daoFactory = new DAOFactory() : daoFactory;
+    }
+    public SuperDAO getDAOType(DAOType daoType){
+        switch (daoType){
+            case ROOM:
+                return new RoomDAOImpl();
+            case STUDENT:
+                return new StudentDAOImpl();
+            case RESERVATION:
+                return new ReservationDAOImpl();
+            case USER:
+                return new UserDAOImpl();
+            default:
+                return null;
+        }
+    }
+}
