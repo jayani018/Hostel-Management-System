236312184f38472112dda231167b57e10fdad6e9
jayani018 <jayaniarunika11@gmail.com>
2023-04-05 00:34:05 +0530

    Create BOFactory, BOType Class

---
+++ src/lk/ijse/gdse62/hibernet/bo/BOFactory.java
@@ -0,0 +1,34 @@
+package lk.ijse.gdse62.hibernet.bo;
+
+import lk.ijse.gdse62.hibernet.bo.custom.UserBO;
+import lk.ijse.gdse62.hibernet.bo.custom.impl.ReservationBOImpl;
+import lk.ijse.gdse62.hibernet.bo.custom.impl.RoomBOImpl;
+import lk.ijse.gdse62.hibernet.bo.custom.impl.StudentBOImpl;
+import lk.ijse.gdse62.hibernet.bo.custom.impl.UserBOImpl;
+
+public class BOFactory {
+    private static BOFactory boFactory;
+
+    private BOFactory(){}
+
+    private static BOFactory getInstance(){
+        return boFactory==null ? boFactory = new BOFactory() :boFactory;
+    }
+
+    public SuperBO getBOType(BOType boType){
+        switch (boType){
+            case USER:
+                return (SuperBO) new UserBOImpl();
+            case STUDENT:
+                return (SuperBO) new StudentBOImpl();
+            case ROOM:
+                return (SuperBO) new RoomBOImpl();
+            case RESERVATION:
+                return (SuperBO) new ReservationBOImpl();
+            default:
+                return null;
+
+        }
+    }
+}
+
---
+++ src/lk/ijse/gdse62/hibernet/bo/BOType.java
@@ -0,0 +1,5 @@
+package lk.ijse.gdse62.hibernet.bo;
+
+public enum BOType {
+    USER,ROOM,STUDENT,RESERVATION
+}
