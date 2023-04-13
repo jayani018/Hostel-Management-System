2c13e4b59c3470e223cab89948e3e6ab38ce8018
jayani018 <jayaniarunika11@gmail.com>
2023-04-14 00:35:02 +0530

    Created Setting Form

---
+++ src/lk/ijse/gdse62/hibernet/controller/SettingFormController.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse62.hibernet.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
+public class SettingFormController {
+    public JFXTextField txtUName;
+    public JFXTextField txtReNewPassword;
+    public JFXTextField txtNewPassword;
+
+    public void ChangeOnAction(ActionEvent actionEvent) {
+    }
+}
