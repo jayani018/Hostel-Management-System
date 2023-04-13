dc6744bc5f7f5f56a36880ed710ce0dbe9f9cc0c
jayani018 <jayaniarunika11@gmail.com>
2023-04-14 01:22:00 +0530

    Created Payments  Form

---
+++ src/lk/ijse/gdse62/hibernet/controller/PaymentsDetailsFormController.java
@@ -0,0 +1,64 @@
+package lk.ijse.gdse62.hibernet.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+import java.awt.event.ActionEvent;
+
+public class PaymentsDetailsFormController {
+
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private TextField search;
+
+    @FXML
+    private TableView<?> Table;
+
+    @FXML
+    private TableColumn<?, ?> tblSID;
+
+    @FXML
+    private TableColumn<?, ?> tblName;
+
+    @FXML
+    private TableColumn<?, ?> tblAddress;
+
+    @FXML
+    private TableColumn<?, ?> tblContact;
+
+    @FXML
+    private TableColumn<?, ?> tblID;
+
+    @FXML
+    private TableColumn<?, ?> tblGender;
+
+    @FXML
+    private TableColumn<?, ?> tbleBID;
+
+
+    public void RoomsOnAction(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void regOnAction(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void StudentOnAction(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void LogoutOnAction(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void PaymentsaOnAction(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void SettingsOnAction(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void SearchOnAction(javafx.event.ActionEvent actionEvent) {
+    }
+}
