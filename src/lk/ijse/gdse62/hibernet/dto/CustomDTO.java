14eaa71bfbb901fc2c674c80e0af82256048c0b0
jayani018 <jayaniarunika11@gmail.com>
2023-04-14 13:23:59 +0530

    Created Custom Class

---
+++ src/lk/ijse/gdse62/hibernet/dto/CustomDTO.java
@@ -0,0 +1,42 @@
+package lk.ijse.gdse62.hibernet.dto;
+
+import lk.ijse.gdse62.hibernet.entity.Room;
+import lk.ijse.gdse62.hibernet.entity.Student;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CustomDTO {
+    private String res_id;
+    private LocalDate date;
+    private Student student;
+    private Room room;
+    private String status;
+    private String room_type_id;
+    private String type;
+    private double key_money;
+    private int qty;
+    private String student_id;
+    private String name;
+    private String address;
+    private String contact_no;
+    private LocalDate dob;
+    private String gender;
+    private String userName;
+    private String password;
+
+    public CustomDTO(String student_id, String name, String address, String contact_no, LocalDate dob, String gender) {
+        this.student_id = student_id;
+        this.name = name;
+        this.address = address;
+        this.contact_no = contact_no;
+        this.dob = dob;
+        this.gender = gender;
+
+    }
+}
---
+++ src/lk/ijse/gdse62/hibernet/entity/Custom.java
@@ -0,0 +1,40 @@
+package lk.ijse.gdse62.hibernet.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Custom {
+    private String res_id;
+    private LocalDate date;
+    private Student student;
+    private Room room;
+    private String status;
+    private String room_type_id;
+    private String type;
+    private double key_money;
+    private int qty;
+    private String student_id;
+    private String name;
+    private String address;
+    private String contact_no;
+    private LocalDate dob;
+    private String gender;
+    private String userName;
+    private String password;
+
+    public Custom(String student_id, String name, String address, String contact_no, LocalDate dob, String gender) {
+        this.student_id = student_id;
+        this.name = name;
+        this.address = address;
+        this.contact_no = contact_no;
+        this.dob = dob;
+        this.gender = gender;
+
+    }
+}
